29af50f0aa3eb6bbc3613acce8ac9c9a983ebd0f
Barış Yaşar <75898912+barisyasar@users.noreply.github.com>
2024-08-10 22:07:19 +0300

    Update getCocktails.js

--- src/services/getCocktails.js
+++ src/services/getCocktails.js
@@ -1,14 +1,8 @@
 // getCocktails
 export default async function getCocktails() {
-  return [];
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/cocktails`);
-  const text = await res.text();
   if (!res.ok) {
     throw new Error(`Failed to fetch cocktails: ${text}`);
   }
-  try {
-    return JSON.parse(text);
-  } catch (e) {
-    throw new Error("Failed to parse JSON response");
-  }
+  return res.json();
 }
